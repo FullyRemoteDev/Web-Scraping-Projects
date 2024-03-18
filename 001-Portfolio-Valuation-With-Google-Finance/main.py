baefe45eefc1f48fd10bb0ae61d42bf0a53faf59
Fully Remote Dev <45625449+FullyRemoteDev@users.noreply.github.com>
2024-03-18 17:39:45 +0530

    adds ability to handle other currencies

--- 001-Portfolio-Valuation-With-Google-Finance/main.py
+++ 001-Portfolio-Valuation-With-Google-Finance/main.py
@@ -9,31 +9,47 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def extract_stock_price(headers, base_url, stock_ticker, stock_exchange):
-    url = base_url + stock_ticker + ':' + stock_exchange
+def convert_to_usd(headers, base_url, currency):
+    url = f"{base_url}{currency}-USD"
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'lxml')
+    soup = BeautifulSoup(response.content, "lxml")
 
-    price_div = soup.find('div', {'data-last-price': True})
-    price = float(price_div['data-last-price'])
-    currency = price_div['data-currency-code']
+    exchange_rate_div = soup.find("div", attrs={"data-last-price": True})
+    exchange_rate = float(exchange_rate_div["data-last-price"])
+
+    return exchange_rate
+
+
+def extract_stock_info(headers, base_url, stock_ticker, stock_exchange):
+    url = base_url + stock_ticker + ":" + stock_exchange
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "lxml")
+
+    price_div = soup.find("div", attrs={"data-last-price": True})
+    price = float(price_div["data-last-price"])
+    currency = price_div["data-currency-code"]
+
+    price_in_usd = price
+    if currency != "USD":
+        exchange_rate = convert_to_usd(headers, base_url, currency)
+        price_in_usd = round(price * exchange_rate, 2)
 
     return {
-        'Ticker': stock_ticker,
-        'Exchange': stock_exchange,
-        'Price': price,
-        'Currency': currency
+        "Ticker": stock_ticker,
+        "Exchange": stock_exchange,
+        "Price": price_in_usd,
+        "Currency": currency,
     }
 
 
 def main():
     base_url = "https://www.google.com/finance/quote/"
-
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
     }
 
-    print(extract_stock_price(headers, base_url,"TSLA", "NASDAQ"))
+    print(extract_stock_info(headers, base_url, "TSLA", "NASDAQ"))
+    print(extract_stock_info(headers, base_url, "SHOP", "TSE"))
 
 
 if __name__ == "__main__":
