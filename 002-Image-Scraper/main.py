044a466d6cbff0bc7198400d0f6329e0e983176f
Fully Remote Dev <45625449+FullyRemoteDev@users.noreply.github.com>
2024-03-24 10:10:38 +0530

    adds the scope statement for the project

---
+++ 002-Image-Scraper/main.py
@@ -0,0 +1,22 @@
+# Scope Statement
+#   - write a python script that scrapes images from Unsplash
+#   - https://unsplash.com
+#   - the utility should download the highest resolution image that
+#     pertains to a given keyword
+#   - premium, watermarked images should be excluded
+#   - for extra practice, explore HTML-based scraping in addition to the
+#     API-based route
+#   - sample interface
+#       if __name__ == "__main__":
+#           scrape("water", 10)
+# 
+
+
+
+
+
+
+
+
+
+
