5c4f0251044eb77fb2d103fe99c4c310eb8f0480
Fully Remote Dev <45625449+FullyRemoteDev@users.noreply.github.com>
2024-04-04 17:58:02 +0530

    initial commit

---
+++ 003-Building-A-Configurable-Scraping-Pipeline/main.py
@@ -0,0 +1,17 @@
+# - Scope Statement
+# 	- Learning Outcome #1
+# 		- JavaScript Scraping with Playwright
+# 		- https://store.steampowered.com/specials
+# 			- title
+# 			- link to thumbnail
+# 			- category tags
+# 			- rating
+# 			- number of reviews
+# 			- original price
+# 			- discounted price
+# 			- discount %
+
+
+if __name__ == "__main__":
+    pass
+
