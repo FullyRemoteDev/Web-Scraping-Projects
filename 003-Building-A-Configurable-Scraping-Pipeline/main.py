83e528325c0379d8c5d585858db4affcdb255199
Fully Remote Dev <45625449+FullyRemoteDev@users.noreply.github.com>
2024-04-18 18:05:35 +0530

    adds ability to extract price info

--- 003-Building-A-Configurable-Scraping-Pipeline/main.py
+++ 003-Building-A-Configurable-Scraping-Pipeline/main.py
@@ -38,10 +38,18 @@ if __name__ == "__main__":
             thumbnail = div.css_first('div[class*="CapsuleImageCtn"] img').attributes.get("src")
             tags = [tag.text() for tag in div.css('a[class*="WidgetTag"]')[:5]]
             release_date = div.css_first('div[class*="_3eOdkTDYdWyo_U5-JPeer1"]').text()
+            review_score = div.css_first('a[class*="ReviewScore"] div div').text()
             reviewed_by = div.css_first('div[class*="_1Deyvnxud-VpRoj0-ak-WK"]').text()
+            
+            sale_price = div.css_first('div[class*="Wh0L8EnwsPV_8VAu8TOYr"]').text()
+            original_price = div.css_first('div[class*="_1EKGZBnKFWOr3RqVdnLMRN"]').text()
+
 
             attrs = {
                 "title": title,
+                "original_price": original_price,
+                "sale_price": sale_price,
+                "review_score": review_score,   
                 "reviewed_by": reviewed_by,
                 "release_date": release_date,
                 "tags": tags,
