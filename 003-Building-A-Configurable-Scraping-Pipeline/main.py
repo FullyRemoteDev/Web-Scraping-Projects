2cbda6938f4421e04d89752dc8e7cb59e3fcc998
Fully Remote Dev <45625449+FullyRemoteDev@users.noreply.github.com>
2024-04-18 18:22:24 +0530

    refactors the extraction code into a separate package

--- 003-Building-A-Configurable-Scraping-Pipeline/main.py
+++ 003-Building-A-Configurable-Scraping-Pipeline/main.py
@@ -11,50 +11,43 @@
 # 			- discounted price
 # 			- discount %
 
-from playwright.sync_api import sync_playwright
+from utils.extract import extract_full_body_html
 from selectolax.parser import HTMLParser
 
 url = "https://store.steampowered.com/specials/"
 
 if __name__ == "__main__":
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto(url)
+    html = extract_full_body_html(
+        from_url=url,
+        wait_for='div[class*="y9MSdld4zZCuoQpRVDgMm"]'
+    )
+
+    tree = HTMLParser(html)
+
+    divs = tree.css('div[class*="y9MSdld4zZCuoQpRVDgMm"]')
+    print(len(divs))
+
+    for div in divs:
+        title = div.css_first('div[class*="StoreSaleWidgetTitle"]').text()
+        thumbnail = div.css_first('div[class*="CapsuleImageCtn"] img').attributes.get("src")
+        tags = [tag.text() for tag in div.css('a[class*="WidgetTag"]')[:5]]
+        release_date = div.css_first('div[class*="_3eOdkTDYdWyo_U5-JPeer1"]').text()
+        review_score = div.css_first('a[class*="ReviewScore"] div div').text()
+        reviewed_by = div.css_first('div[class*="_1Deyvnxud-VpRoj0-ak-WK"]').text()
         
-        page.wait_for_load_state("networkidle")
-        page.evaluate("() => window.scrollTo(0, document.body.scrollHeight)")
-        page.wait_for_load_state("domcontentloaded")
-        page.wait_for_selector('div[class*="y9MSdld4zZCuoQpRVDgMm"]')
-
-        html = page.inner_html("body")
-        tree = HTMLParser(html)
-
-        divs = tree.css('div[class*="y9MSdld4zZCuoQpRVDgMm"]')
-        print(len(divs))
-
-        for div in divs:
-            title = div.css_first('div[class*="StoreSaleWidgetTitle"]').text()
-            thumbnail = div.css_first('div[class*="CapsuleImageCtn"] img').attributes.get("src")
-            tags = [tag.text() for tag in div.css('a[class*="WidgetTag"]')[:5]]
-            release_date = div.css_first('div[class*="_3eOdkTDYdWyo_U5-JPeer1"]').text()
-            review_score = div.css_first('a[class*="ReviewScore"] div div').text()
-            reviewed_by = div.css_first('div[class*="_1Deyvnxud-VpRoj0-ak-WK"]').text()
-            
-            sale_price = div.css_first('div[class*="Wh0L8EnwsPV_8VAu8TOYr"]').text()
-            original_price = div.css_first('div[class*="_1EKGZBnKFWOr3RqVdnLMRN"]').text()
+        sale_price = div.css_first('div[class*="Wh0L8EnwsPV_8VAu8TOYr"]').text()
+        original_price = div.css_first('div[class*="_1EKGZBnKFWOr3RqVdnLMRN"]').text()
 
 
-            attrs = {
-                "title": title,
-                "original_price": original_price,
-                "sale_price": sale_price,
-                "review_score": review_score,   
-                "reviewed_by": reviewed_by,
-                "release_date": release_date,
-                "tags": tags,
-                "thumbnail": thumbnail,
-            }
+        attrs = {
+            "title": title,
+            "original_price": original_price,
+            "sale_price": sale_price,
+            "review_score": review_score,   
+            "reviewed_by": reviewed_by,
+            "release_date": release_date,
+            "tags": tags,
+            "thumbnail": thumbnail,
+        }
 
-            print(attrs)
-
+        print(attrs)
